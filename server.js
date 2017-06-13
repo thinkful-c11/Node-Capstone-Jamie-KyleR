0a3ec1724593ea1ab9582691a9173e1e02560fa9
Kyle Rogers <kylerogers334@gmail.com>
2017-06-13 19:14:32 +0000

    Merged in portfolio router and added some config

---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {TEST_DATABASE_URL, PORT} = require('./config/config');
+const portfolioRouter = require('./src/js/routes/portfolioRouter');
+const securityRouter = require('./src/js/routes/securityRouter');
+
+const app = express();
+app.use('/portfolio', portfolioRouter);
+app.use('/security', securityRouter);
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+let server;
+
+function runServer(databaseUrl=TEST_DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {runServer, app, closeServer};
+
