8c39a77a779c72981832951b06a4ec5c61d43da6
Jamie Wang <sephona@gmail.com>
2017-06-16 12:07:01 -0400

    merged in function from master branch

--- src/js/frontend-helpers.js
+++ src/js/frontend-helpers.js
@@ -9,6 +9,19 @@ function formatMoney(m) {
       currency: 'USD' });
 }
 
+function unFormatMoney(m) {
+  return Number(m.replace(/[^0-9\.]+/g,""));
+}
+
+
+///this is setting the dashboard to name and values to equal what was submitted///
+function setDashboard() {
+  $('#portfolio-name').html(portfolio.name);
+  $('#portfolio-value').html(formatMoney(portfolio.value));
+  $('#view-link').append(`<a href="/portfolio/${portfolio.link}">Dashboard</a>`);
+  $('#chart-link').append(`<a href="/portfolio/${portfolio.link}/charts">Charts</a>`);
+  $('#trade-link').append(`<a href="/portfolio/${portfolio.link}/trade">Trade</a>`);
+}
 
 ///this is setting the dashboard to name and values to equal what was submitted///
 function setDashboard() {
